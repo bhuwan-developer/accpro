9043fc28a7ba255e1afe758140c5b7b38d2502b3
unknown <bhuwan.bbsoft@gmail.com>
2024-11-12 14:54:06 +0530

    initial commit

---
+++ src/main/java/com/bbsoft/accpro/AccproApplication.java
@@ -0,0 +1,13 @@
+package com.bbsoft.accpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bbsoft/accpro/AccproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbsoft.accpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
